8d497f273ce369bc22b03b266da08cae5b8e8d98
Yves Goergen <yves@unclassified.de>
2014-02-22 19:17:10 +0100

    Log viewer recognises path notification and offers loading those files
    * The debug monitor must be active to catch the DebugOutputString message
    * A button in the details view for that message offers loading the indicated files

--- FieldLogViewer/ViewModel/DebugMessageViewModel.cs
+++ FieldLogViewer/ViewModel/DebugMessageViewModel.cs
@@ -5,18 +5,27 @@ using System.Text;
 using System.Windows;
 using System.Windows.Media;
 using Unclassified.FieldLog;
+using Unclassified.UI;
 
 namespace Unclassified.FieldLogViewer.ViewModel
 {
 	class DebugMessageViewModel : LogItemViewModelBase
 	{
+		#region Constructor
+
 		public DebugMessageViewModel(int pid, string text)
 		{
 			this.Time = FL.UtcNow;
 			this.UtcOffset = (int) DateTimeOffset.Now.Offset.TotalMinutes;
 			this.ProcessId = pid;
-			this.Message = text;
+			this.Message = (text ?? "").TrimEnd();
+
+			InitializeCommands();
 		}
+
+		#endregion Constructor
+
+		#region Properties
 
 		public int ProcessId { get; private set; }
 		public string Message { get; private set; }
@@ -79,5 +88,47 @@ namespace Unclassified.FieldLogViewer.ViewModel
 			get { return this.isSelected; }
 			set { CheckUpdate(value, ref isSelected, "IsSelected", "Background", "Foreground"); }
 		}
+
+		public Visibility OpenLogFileButtonVisibility
+		{
+			get
+			{
+				return Message.StartsWith("FieldLog info: Now writing to ") ? Visibility.Visible : Visibility.Collapsed;
+			}
+		}
+		
+		#endregion Properties
+
+		#region Commands
+
+		public DelegateCommand OpenLogFileCommand { get; private set; }
+
+		private void InitializeCommands()
+		{
+			OpenLogFileCommand = new DelegateCommand(OnOpenLogFile);
+		}
+
+		private void OnOpenLogFile()
+		{
+			string s = "FieldLog info: Now writing to ";
+			if (Message.StartsWith(s))
+			{
+				try
+				{
+					MainViewModel.Instance.OpenFiles(Message.Substring(s.Length).Trim());
+				}
+				catch (Exception ex)
+				{
+					FL.Error(ex, "Loading log files from DebugOutputString notification");
+					MessageBox.Show(
+						"Error loading the specified log files. " + ex.Message,
+						"FieldLogViewer",
+						MessageBoxButton.OK,
+						MessageBoxImage.Error);
+				}
+			}
+		}
+
+		#endregion Commands
 	}
 }
